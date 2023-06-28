0dd1a049680e66ad5cc0e92038203a04ca5d8e71
Lucas Bento <lucasiguape16@gmail.com>
2023-06-27 21:49:48 -0300

    add create category and select category

--- Program.cs
+++ Program.cs
@@ -22,10 +22,51 @@ using(var connection = new SqlConnection(connectionString))
     //         Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
     //     }
     // };
+    ListCategories(connection);
+    CreateCategory(connection);
+}
 
+static void ListCategories(SqlConnection connection)
+{
     var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-    foreach(var category in categories)
+    foreach(var item in categories)
     {
-        Console.WriteLine($"{category.Id} - {category.Title}");
+        Console.WriteLine($"{item.Id} - {item.Title}");
     }
+}
+
+static void CreateCategory(SqlConnection connection)
+{
+    var category = new Category
+    {
+        Id = Guid.NewGuid(),
+        Title = "Azure",
+        Url = "azure.com",
+        Summary = "Azure",
+        Order = 8,
+        Description = "Serviços Azure",
+        Featured = true
+    };
+
+    var insertSql = @"INSERT INTO [Category] VALUES (
+        @Id, 
+        @Title, 
+        @Url, 
+        @Summary,
+        @Order, 
+        @description, 
+        @featured
+    )";
+
+    var rows = connection.Execute(insertSql, new {
+        category.Id, 
+        category.Title, 
+        category.Url, 
+        category.Summary, 
+        category.Order, 
+        category.Description, 
+        category.Featured
+    });
+
+    Console.WriteLine($"{rows} linha(s) inseridas");
 }
